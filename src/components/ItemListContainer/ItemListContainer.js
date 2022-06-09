92cdb509f270ccf61c4399c49830b25efa1a5d5a
fpatrilla <fpatrilla@gmail.com>
2022-06-09 00:26:40 -0300

    3.1

---
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './ItemListContainer.css'
+
+
+
+
+const ItemListContainer = (props) => {
+
+
+	return (
+		<div className='ItemListContainer'>
+			<h2>{props.title}</h2>
+		
+		</div>
+	);
+};
+
+export default ItemListContainer;
